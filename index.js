0c719c8d87a185cfe6984693cf385c10c320af6a
alomsoltanul <alomsoltanul@gmail.com>
2024-02-24 19:02:37 +0900

    POST function applied

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -29,6 +29,27 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+
+    app.get('/coffee' , async(req,res) =>{
+      const cursor =coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    app.post('/coffee', async (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+    app.delete('/coffee/:id', async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -42,9 +63,9 @@ run().catch(console.dir);
 // Adding MONDODB
 
 app.get('/', (req, res) => {
-    res.send('coffee making server is live')
+  res.send('coffee making server is live')
 })
 
 app.listen(port, () => {
-    console.log(`coffee server is running on pc, ${port}`);
+  console.log(`coffee server is running on pc, ${port}`);
 })
